a898c7377509a43e5b887a4e489a3074f69175c1
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-10 23:16:05 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_hello(name="Ruby programmer!")
+  puts "Hello #{name}"
+>>>>>>> 3d506cd609ea1756641446c453e362597b47c9e2
 end
